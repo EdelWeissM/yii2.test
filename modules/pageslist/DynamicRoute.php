47367596b3dc2a14f0c1ec3087d6f7bdc5e12a00
EdelWeissM <makxiss@gmail.com>
2017-12-20 16:56:42 +0200

    Max commit: add main changes for Route

--- modules/pageslist/DynamicRoute.php
+++ modules/pageslist/DynamicRoute.php
@@ -12,37 +12,53 @@ use yii\base\BaseObject;
 use yii\web\Request;
 use yii\web\UrlManager;
 use yii\web\UrlRuleInterface;
+use app\modules\pageslist\models\Page;
 
 class DynamicRoute extends BaseObject implements UrlRuleInterface
 {
 
-
     /**
-     * Parses the given request and returns the corresponding route and parameters.
+     * * Parses the given request and returns the corresponding route and parameters.
      * @param UrlManager $manager the URL manager
      * @param Request $request the request component
-     * @return array|bool the parsing result. The route and the parameters are returned as an array.
+     * @return array|bool
      * If false, it means this rule cannot be used to parse this path info.
      */
     public function parseRequest($manager, $request)
     {
         $pathInfo = $request->getPathInfo();
-        if (preg_match('%^(\w+)(/(\w+))?$%', $pathInfo, $matches)) {
+        $matches = explode("/", $pathInfo);
+
+        if (count($matches)) {
+            $parent_id = null;
+            foreach ($matches as $url){
+                $page = Page::find()->where([
+                    'url' => $url,
+                    'hidden' => 0,
+                    'parent_id'=> $parent_id
+                ])->one();
+                if (!$page) {
+                    return false;
+                }
+                $parent_id = $page->parent_id;
+            }
+            return ['pageslist/list/page', ['article' => $page]];
         }
         return false;
     }
 
+
     /**
-     * Creates a URL according to the given route and parameters.
+     * * Creates a URL according to the given route and parameters.
      * @param UrlManager $manager the URL manager
      * @param string $route the route. It should not have slashes at the beginning or the end.
      * @param array $params the parameters
-     * @return string|bool the created URL, or false if this rule cannot be used for creating this URL.
+     * @return array|bool the created URL, or false if this rule cannot be used for creating this URL.
      */
     public function createUrl($manager, $route, $params)
     {
-        if ($route === 'pagelist/list/index') {
-                return $params['url'];
+        if ($route === 'pageslist/list/page') {
+            return $params['article']['url'];
         }
         return false;  // данное правило не применимо
     }
